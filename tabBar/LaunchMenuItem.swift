5b3650164456d65e2f93b64678404b5d66bbd2b3
lilei <lilei19891030@qq.com>
2017-02-11 12:44:08 +0800

    add files

---
+++ tabBar/LaunchMenuItem.swift
@@ -0,0 +1,33 @@
+//
+//  SKLaunchMenuItem.swift
+//  SCNavController
+//
+//  Created by lilei on 16/9/1
+//
+
+import UIKit
+
+class LaunchMenuItem: UICollectionViewCell {
+    
+    var titleLabel:UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: kScreenWidth / 5, height: 30))
+//        titleLabel.backgroundColor = UIColor.groupTableViewBackgroundColor()
+        titleLabel.backgroundColor = Constants.themeColor
+
+        titleLabel.font = UIFont.systemFont(ofSize: 14)
+        titleLabel.textAlignment = NSTextAlignment.center
+        
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 5
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 0.2
+        self.contentView.addSubview(titleLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ tabBar/NavParams.swift
@@ -0,0 +1,38 @@
+//
+//  NavParams.swift
+//  demo1
+//
+//  Created by lilei on 16/9/1.
+//  Copyright © 2016年 lilei. All rights reserved.
+//
+
+
+import UIKit
+
+let kScreenWidth = UIScreen.main.bounds.size.width
+let kScreenHeight = UIScreen.main.bounds.size.height
+/**
+ * 屏幕状态栏的高度
+ */
+let kStateBarHeight:CGFloat = UIScreen.main.bounds.origin.y
+/**
+ * 系统导航控制器的高度
+ */
+//let kNavBarHeight:CGFloat = kScreenHeight
+/**
+ * 展开扩展菜单按钮的宽度
+ */
+let kArrowButtonWidth:CGFloat = 40    //可以在这改变导航栏的高度
+/**
+ * 自定义滑动导航栏的高度
+ */
+let kScNavBarHeight:CGFloat = 40
+/**
+ * 自定义导航栏的颜色
+ */
+let kNavColor = Constants.themeColor  //这个是在不设置自定义导航栏的颜色时的默认颜色
+
+class NavParams: NSObject {
+    
+}
+
